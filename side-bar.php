a4295dce2c26171f9fe26b89ba1ce35cfefea4f1
Osama Mumtaz <osamamumtaz96@gmail.com>
2019-04-27 22:43:04 +0500

    Update side-bar.php
    Categories in sidebar are updating from SQL now.

--- side-bar.php
+++ side-bar.php
@@ -1,11 +1,25 @@
+<?php
+require_once("connection.php");
+$connection = mysqli_connect(DBHOST, DBUSER, DBPASSWORD, DBNAME);
+if (mysqli_connect_error()) {
+    die(mysqli_connect_error());
+} else { }
+
+
+
+?>
 <div class="sidebar">
     <h4 class="sidebar-heading">Categories</h4>
     <ul>
-        <li><a href="./shirts.php">Shirts</a></li>
-        <li><a href="./pents.php">Pents</a></li>
-        <li><a href="#">Trousers</a></li>
-        <li><a href="#">Suits</a></li>
-        <li><a href="#">Shoes</a></li>
+        <?php
+        $sql = "SELECT category FROM productcategory ORDER BY category";
+        $result = mysqli_query($connection, $sql);
+        $select = "";
+        while ($row = mysqli_fetch_assoc($result)) {
+            $select .= '<li><a href="./' . $row['category'] . '.php">' . $row['category'] . '</a></li>';
+        }
+        echo $select;
+        ?>
     </ul>
 </div>
 <div class="clear"></div>
