a66e5c2533d99006d0dd4939c48eccbea360b309
Osama Mumtaz <osamamumtaz96@gmail.com>
2019-04-29 20:38:38 +0500

    Add files via upload

--- side-bar.php
+++ side-bar.php
@@ -4,19 +4,16 @@ $connection = mysqli_connect(DBHOST, DBUSER, DBPASSWORD, DBNAME);
 if (mysqli_connect_error()) {
     die(mysqli_connect_error());
 } else { }
-
-
-
 ?>
 <div class="sidebar">
     <h4 class="sidebar-heading">Categories</h4>
     <ul>
         <?php
-        $sql = "SELECT category FROM productcategory ORDER BY category";
+        $sql = "SELECT * FROM productcategory ORDER BY category";
         $result = mysqli_query($connection, $sql);
         $select = "";
         while ($row = mysqli_fetch_assoc($result)) {
-            $select .= '<li><a href="./' . $row['category'] . '.php">' . $row['category'] . '</a></li>';
+            $select .= '<li><a href="./categoryDisplay.php?id='. $row['id'] . '&category='. $row['category'] .'">' . $row['category'] . '</a></li>';
         }
         echo $select;
         ?>
